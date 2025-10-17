d613aaa6ed26313726f13e62e7d6ce2b111a14cc
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2025-10-17 08:06:40 +0300

    Edit repeat constructor

--- Beverage/Beverage/Beverages/Coffee.cs
+++ Beverage/Beverage/Beverages/Coffee.cs
@@ -1,31 +1,26 @@
 ﻿namespace Beverages;
 public abstract class Coffee : BaseBeverage
 {
-    protected Coffee( string description ) : base( description ) { }
+    protected readonly Size _size;
+    protected Coffee( string type, Size size )
+        : base( $"{( size == Size.Double ? "Double" : "Standard" )} {type}" )
+    {
+        _size = size;
+    }
 }
 
 public enum Size { Standard, Double }
 
 public class Latte : Coffee
 {
-    private readonly Size _size;
-    public Latte( Size size = Size.Standard )
-        : base( $"{( size == Size.Double ? "Double" : "Standard" )} Latte" )
-    {
-        _size = size;
-    }
+    public Latte( Size size = Size.Standard ): base( "Latte", size ) {}
 
     public override double GetCost() => _size == Size.Double ? 130 : 90;
 }
 
 public class Cappuccino : Coffee
 {
-    private readonly Size _size;
-    public Cappuccino( Size size = Size.Standard )
-        : base( $"{( size == Size.Double ? "Double" : "Standard" )} Cappuccino" )
-    {
-        _size = size;
-    }
+    public Cappuccino( Size size = Size.Standard ): base( "Cappuccino", size ){}
 
     public override double GetCost() => _size == Size.Double ? 120 : 80;
 }
