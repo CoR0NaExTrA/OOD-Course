766ab99c4d9977812e5883fa33a9ea4283e43185
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2025-10-23 21:55:30 +0300

    Add MemoryStream

---
+++ Streams/Streams/Streams/MemoryInputStream.cs
@@ -0,0 +1,36 @@
+﻿namespace Transform.Streams;
+public class MemoryInputStream : IInputDataStream
+{
+    private readonly byte[] _data;
+    private int _position = 0;
+
+    public MemoryInputStream( byte[] data )
+    {
+        _data = data ?? throw new ArgumentNullException( nameof( data ) );
+    }
+
+    public bool IsEOF => _position >= _data.Length;
+
+    public byte ReadByte()
+    {
+        if ( IsEOF )
+            throw new InvalidOperationException( "End of stream reached" );
+        return _data[ _position++ ];
+    }
+
+    public int ReadBlock( byte[] dstBuffer, int size )
+    {
+        if ( IsEOF )
+            return 0;
+
+        int remaining = _data.Length - _position;
+        int toRead = Math.Min( size, remaining );
+        Array.Copy( _data, _position, dstBuffer, 0, toRead );
+        _position += toRead;
+        return toRead;
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ Streams/Streams/Streams/MemoryOutputStream.cs
@@ -0,0 +1,32 @@
+﻿namespace Transform.Streams;
+public class MemoryOutputStream : IOutputDataStream
+{
+    private readonly List<byte> _buffer = new();
+    private bool _closed = false;
+
+    public IReadOnlyList<byte> Data => _buffer;
+
+    public void WriteByte( byte data )
+    {
+        if ( _closed )
+            throw new InvalidOperationException( "Stream is closed" );
+        _buffer.Add( data );
+    }
+
+    public void WriteBlock( byte[] srcData, int size )
+    {
+        if ( _closed )
+            throw new InvalidOperationException( "Stream is closed" );
+        for ( int i = 0; i < size; i++ )
+            _buffer.Add( srcData[ i ] );
+    }
+
+    public void Close()
+    {
+        _closed = true;
+    }
+
+    public void Dispose()
+    {
+    }
+}
